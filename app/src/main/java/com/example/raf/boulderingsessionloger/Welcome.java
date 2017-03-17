2b9651211b28acc4c188cb4217203d9ac3f3b143
Raf <rkrassowski@gmail.com>
2017-03-17 12:16:01 +0000

    initial implementation of recycle/card view for session history and cosmetic tweaks

--- app/src/main/java/com/example/raf/boulderingsessionloger/Welcome.java
+++ app/src/main/java/com/example/raf/boulderingsessionloger/Welcome.java
@@ -1,30 +1,78 @@
 package com.example.raf.boulderingsessionloger;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Welcome extends AppCompatActivity {
 
-    public void goToLogSession(View view){
+    private List<Session> sessions;
+    private RecyclerView recyclerView;
+
+    public void goToLogSession(View view) {
 
         Intent intent = new Intent(Welcome.this, Log_session.class);
         startActivity(intent);
 
     }
 
-    public void goToViewHistory(View view){
+    public void goToViewHistory(View view) {
 
         Intent intent = new Intent(Welcome.this, View_history.class);
         startActivity(intent);
 
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(llm);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(sessions);
+        recyclerView.setAdapter(adapter);
+
+        initializeData();
+        initializeAdapter();
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            // do stuff with the user
+        } else {
+            Intent intent = new Intent(Welcome.this, Login.class);
+            startActivity(intent);
+        }
+    }
+
+
+    private void initializeData() {
+        sessions = new ArrayList<>();
+        sessions.add(new Session("12 sends", "V4 average"));
+        sessions.add(new Session("3 sends", "V6 average"));
+        sessions.add(new Session("21 sends", "V3 average"));
+        sessions.add(new Session("12 sends", "V4 average"));
+        sessions.add(new Session("3 sends", "V6 average"));
+        sessions.add(new Session("21 sends", "V3 average"));
+        sessions.add(new Session("12 sends", "V4 average"));
+        sessions.add(new Session("3 sends", "V6 average"));
+        sessions.add(new Session("21 sends", "V3 average"));
+    }
+
+    private void initializeAdapter() {
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(sessions);
+        recyclerView.setAdapter(adapter);
     }
 }
